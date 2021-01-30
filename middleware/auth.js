cf6dd3485da39464ed3208954d9258d1765a43a5
unknown <bymbot@gmail.com>
2021-01-30 19:58:54 +0200

    Backnd\

---
+++ middleware/auth.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken'
+
+
+// Хочет лайкнуть пост
+// => auth middleware (next) => lik controller
+
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+
+        const isCustomAuth = token.length < 500
+
+        let decodedData
+
+        if (token && isCustomAuth) {
+            decodedData = jwt.verify(token, process.env.JWT_KEY)
+
+            req.userId = decodedData?.id
+        }
+        else {
+            decodedData = jwt.decode(token)
+
+            req.userId = decodedData?.sub
+        }
+
+        next()
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
+
+export default auth
